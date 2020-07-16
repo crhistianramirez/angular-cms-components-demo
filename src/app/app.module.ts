eb00224048657a75d49505271cd8625a16ce4e1d
Crhistian Ramirez <cramirez@four51.com>
2020-07-16 11:46:55 -0500

    add @ordercloud/angular-cms-components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { CmsAdminModule } from '@ordercloud/angular-cms-components';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +8,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CmsAdminModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
